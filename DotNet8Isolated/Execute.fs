edb9f162b33ee107cdb7fd1b32a9fb9d337e4774
kumkee <kumkee@users.noreply.github.com>
2024-04-19 20:50:55 +1200

    first commit

--- DotNet8Isolated/Execute.fs
+++ DotNet8Isolated/Execute.fs
@@ -3,17 +3,23 @@
 open Microsoft.Azure.Functions.Worker
 open Microsoft.Extensions.Logging
 open Microsoft.Azure.Functions.Worker.Http
+open Microsoft.AspNetCore.Http
+open Microsoft.AspNetCore.Http.Extensions
 open System.Net
 
-type Execute(logger: ILogger<Execute>) =
-    
-    [<Function("Execute")>]        
-    member _.Run([<HttpTrigger()>]  req:HttpRequestData, executionContext:FunctionContext) =
+type Execute(logger: ILogger<Execute>, reqHttp: HttpRequest<Execute>) =
+
+    [<Function("Execute")>]
+    member _.Run
+        ([<HttpTrigger(AuthorizationLevel.Function, "get")>] req:
+            HttpRequestData)
+        =
         task {
-            logger.LogInformation($"Hello at {System.DateTime.UtcNow} from an Azure function using F# on .NET 8.")
+            logger.LogInformation
+                $"Hello at {System.DateTime.UtcNow} from an Azure function using F# on .NET 8."
+
             let response = req.CreateResponse(HttpStatusCode.OK)
-            do! response.WriteStringAsync("Hello World")
+            let url = reqHttp.GetDisplayUrl ()
+            do! response.WriteStringAsync <| "Hello World " + url
             return response
         }
-        
-
